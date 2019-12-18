892a9f376913a27a294038ef1f48eae814ca740a
Code4Blessings <robinwarden@code4blessings.com>
2019-12-18 08:59:54 -0500

    Updated the login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/friends'>Friends Page(Protected)</Link>
+        </li>
+      </ul>
       <h1>FRIENDS!!!</h1>
+      <Route path="/login" component={Login} />
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            credentials: {
+                username: '',
+                password: ''
+            }
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth().post('/login')
+    }
+
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.login} className="form-container" >
+                    <label htmlFor="username">Username:
+                        <input 
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username} 
+                        onChange={this.handleChange} />
+                    </label>
+
+                    <label htmlFor="password">Password:
+                        <input 
+                        type="text"
+                        name="password"
+                        value={this.state.credentials.username} 
+                        onChange={this.handleChange} />
+                    </label>
+                    <button>Login</button>
+                </form>
             </div>
         );
     }
